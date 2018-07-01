bdc3b26b6917f3b42ea86cf15227d9d7df1fd90d
João Paulo <jpsc0908@gmail.com>
2018-06-30 21:04:03 -0300

    métodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,31 @@
 
 require_once("config.php");
 
-$paulo = new Usuario();
 
+//Carrega um usuário
+/*
+$paulo = new Usuario();
 $paulo->loadById(4);
 
 echo $paulo;
+*/
+
+//carrega uma lista de usuários
+
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+
+//$search = Usuario::search("joao");
+//echo json_encode($search);
+
+
+//Carrega um usuário, usando um login e uma senha
+
+$usuario=new Usuario();
+$usuario->login("joao","ruti");
+echo $usuario;
+
 
  ?>
